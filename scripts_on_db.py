914be5198124dd30c28f30d2d0d22fa2e3267630
IPodstolnyy <ipodstolnyy@spb.beeline.ru>
2025-05-27 09:40:28 +0300

    I added def get_lesson and replace main

--- scripts_on_db.py
+++ scripts_on_db.py
@@ -38,39 +38,13 @@ TEXTS = [
     ]
 
 
-def fix_marks(schoolkid):
-    return Mark.objects.filter(schoolkid=schoolkid,
-                               points__in=[2, 3]).update(points=5)
-
-
-def remove_chastisements(schoolkid):
-    return Chastisement.objects.filter(
-        schoolkid=schoolkid).delete()
-
-
-def create_commendation(schoolkid, subject):
-    lesson = Lesson.objects.filter(
-        year_of_study=schoolkid.year_of_study,
-        group_letter=schoolkid.group_letter,
-        subject=subject).order_by('-date').first()
-
-    if not Commendation.objects.filter(schoolkid=schoolkid,
-                                       subject=subject).first():
-        return Commendation.objects.create(
-            text=random.choice(TEXTS),
-            created=lesson.date,
-            schoolkid=schoolkid,
-            subject=subject,
-            teacher=lesson.teacher,)
-
-
 def get_user(FULL_NAME):
     try:
-        return Schoolkid.objects.get(full_name__contains=FULL_NAME)
-    except Subject.MultipleObjectsReturned:
-        Schoolkid.objects.filter(full_name__contains=FULL_NAME).first()
+        return Schoolkid.objects.get(full_name=FULL_NAME)
+    except Schoolkid.MultipleObjectsReturned:
+        return Schoolkid.objects.filter(full_names=FULL_NAME).first()
     except Schoolkid.DoesNotExist:
-        return None
+        return False
 
 
 def get_subject(FULL_NAME, TITLE):
@@ -83,27 +57,69 @@ def get_subject(FULL_NAME, TITLE):
             year_of_study=get_user(FULL_NAME).year_of_study,
             title=TITLE).first()
     except Subject.DoesNotExist:
-        return None
+        return False
+    except AttributeError:
+        return False
+
+
+def get_lesson(FULL_NAME, TITLE):
+    try:
+        return Lesson.objects.filter(
+            year_of_study=get_user(FULL_NAME).year_of_study,
+            group_letter=get_user(FULL_NAME).group_letter,
+            subject=get_subject(FULL_NAME, TITLE)).order_by('-date').first()
+    except Lesson.DoesNotExist:
+        return False
+    except AttributeError:
+        return False
+
+
+def fix_marks(schoolkid):
+    return Mark.objects.filter(schoolkid=schoolkid,
+                               points__in=[2, 3]).update(points=5)
+
+
+def remove_chastisements(schoolkid):
+    return Chastisement.objects.filter(
+        schoolkid=schoolkid).delete()
+
+
+def create_commendation(schoolkid, subject):
+
+    if not Commendation.objects.filter(schoolkid=schoolkid,
+                                       subject=subject).first():
+        return Commendation.objects.create(
+                text=random.choice(TEXTS),
+                created=get_lesson(schoolkid.full_name, subject.title).date,
+                schoolkid=schoolkid,
+                subject=subject,
+                teacher=get_lesson(schoolkid.full_name,
+                                   subject.title).teacher,)
 
 
 def main():
     FULL_NAME = input(str("Введити ФИО ученика:"))
     TITLE = input(str("Введите учебный предмет:"))
 
-    if get_user(FULL_NAME) is None:
-        print(f'Ученик с именем "{FULL_NAME}" не найден.')
-    elif get_subject(FULL_NAME, TITLE) is None:
-        print(f'Учебный предмет "{TITLE}" не найден.')
+    schoolkid = get_user(FULL_NAME)
+    subject = get_subject(FULL_NAME, TITLE)
+    lesson = get_lesson(FULL_NAME, TITLE)
+
+    if get_user(FULL_NAME):
+        fix_marks(schoolkid)
+        print("Плохие оценки удалены!")
+        if subject:
+            remove_chastisements(schoolkid)
+            print("Плохие комментарии удалены!")
+            if lesson:
+                create_commendation(schoolkid, subject)
+                print("К уроку добавлен положительный комментрий!")
+            else:
+                print("Урок не найден!")
+        else:
+            print(f'Учебный предмет {TITLE} не найден!')
     else:
-        fix_marks(get_user(FULL_NAME))
-        remove_chastisements(get_user(FULL_NAME))
-        create_commendation(get_user(FULL_NAME),
-                            get_subject(FULL_NAME, TITLE))
-        print("""
-Из журнала удалены двойки и тройки
-Плохие комментарии от учителя удалены
-Положительный комментарий к уроку добавлен
-              """)
+        print(f'Ученик с именем {FULL_NAME} не найден!')
 
 
 if __name__ == '__main__':
